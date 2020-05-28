e4d7d4483d4fe8de661c94e554027b4163be569f
talyohai <talko@cervellosec.com>
2020-05-28 14:30:59 +0300

    Delete dist

--- dist/src/components/Ports/Ports.default.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { IConfig, INode } from '../../';
-export interface IPortsDefaultProps {
-    config: IConfig;
-    node: INode;
-    children: Array<React.ReactElement<any>>;
-}
-export declare const PortsDefault: ({ children, config }: IPortsDefaultProps) => JSX.Element;
