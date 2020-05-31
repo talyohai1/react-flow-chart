1ab4c6de7bc8b59c5891b31b14f1fb4ab5e8500f
talyohai <talko@cervellosec.com>
2020-05-31 16:23:55 +0300

    Delete dist to re-add later

--- dist/src/components/Link/utils/generateCurvePath.d.ts
+++
@@ -1,4 +0,0 @@
-import { IPort, IPosition } from '../../../';
-export declare const generateCurvePath: (startPos: IPosition, endPos: IPosition) => string;
-export declare const generateRightAnglePath: (startPos: IPosition, endPos: IPosition) => string;
-export declare const generateSmartPath: (matrix: number[][], startPos: IPosition, endPos: IPosition, fromPort: IPort, toPort: IPort) => string;
